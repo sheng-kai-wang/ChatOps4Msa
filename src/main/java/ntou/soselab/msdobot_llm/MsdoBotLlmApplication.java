503f05a0941915686919abe4c92f91c3a8a18651
sheng-kai-wang <nssh94879487@gmail.com>
2023-05-13 17:53:33 +0800

    first commit

---
+++ src/main/java/ntou/soselab/msdobot_llm/MsdoBotLlmApplication.java
@@ -0,0 +1,12 @@
+package ntou.soselab.msdobot_llm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsdoBotLlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsdoBotLlmApplication.class, args);
+    }
+}
---
+++ src/test/java/ntou/soselab/msdobot_llm/MsdoBotLlmApplicationTests.java
@@ -0,0 +1,13 @@
+package ntou.soselab.msdobot_llm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsdoBotLlmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
